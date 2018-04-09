125208278d2e305a613dbf75473af1f1380119fe
phongvt3110 <vtphong651043@gmail.com>
2018-04-09 23:35:17 +0700

    update code

--- src/Model/Table/ArticlesTable.php
+++ src/Model/Table/ArticlesTable.php
@@ -10,11 +10,19 @@
 namespace App\Model\Table;
 
 use Cake\ORM\Table;
+use Cake\Utility\Text;
 
 class ArticlesTable extends Table
 {
     public function initialize(array $config)
     {
         $this->addBehavior('Timestamp');
+    }
+
+    public function beforeSave($event, $entity, $option){
+        if($entity->isNew() && !$entity->slug){
+            $sluggedTitle = Text::slug($entity->title);
+            $entity->slug = substr($sluggedTitle, 0, 191);
+        }
     }
 }
--- src/Template/Articles/index.ctp
+++ src/Template/Articles/index.ctp
@@ -4,6 +4,7 @@
     <tr>
         <th>Title</th>
         <th>Created</th>
+        <th></th>
     </tr>
 
     <!-- Here is where we iterate through our $articles query object, printing out article info -->
@@ -15,6 +16,9 @@
         </td>
         <td>
             <?= $article->created->format(DATE_RFC850) ?>
+        </td>
+        <td>
+            <?= $this->Html->link('Edit', ['action' => 'edit', $article->slug]) ?>
         </td>
     </tr>
     <?php endforeach; ?>
